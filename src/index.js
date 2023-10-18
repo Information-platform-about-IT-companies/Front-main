703bd74be82e5df1633b78a23dd91cebbbe4c83c
vvvolkova-92 <saiviolet@gmail.com>
2023-10-18 17:01:29 +0300

    Установка и настройка husky, lint-staged, eslint плагинов для jest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 import ThemeContextProvider from "./context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
