866f9e3de7f8e1fbfa7cd894c7a2d5bb8c3b009a
Nikita S <->
2023-10-20 11:32:39 +0300

    Реализовать роутинг

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-// eslint-disable-next-line arrow-body-style, react/function-component-definition
-const ProtectedRouteElement = ({ elemnt: Component, ...props }) => {
+function ProtectedRouteElement({ element: Component, ...props }) {
   return props.loggedIn ? <Component {...props} /> : <Navigate to="/signin" replace />;
-};
+}
 
 export default ProtectedRouteElement;
