b9014ae5b5cdea396d1179731d4fb8256775ab71
Alina Sultanova <sultanova1859ts@gmail.com>
2023-10-23 13:54:26 +0500

    Сделала компонент Checkbox и storybook к нему

--- src/UI-KIT/Checkbox/Checkbox.stories.jsx
+++ src/UI-KIT/Checkbox/Checkbox.stories.jsx
@@ -15,11 +15,19 @@ const meta = {
     title: {
       description: "Обязательный параметр. Используйте для title",
     },
+<<<<<<< HEAD
     id: {
       description: "Обязательный параметр. Передаем внутрь id чекбокса",
     },
     name: {
       description: "Обязательный параметр. Передаем внутрь name чекбокса",
+=======
+    onChange: {
+      description: "Обязательный параметр. Передаем внутрь функцию",
+    },
+    value: {
+      description: "Обязательный параметр. Передает значение чекбокса",
+>>>>>>> 2895b2c (Сделала компонент Checkbox и storybook к нему)
     },
   },
 };
@@ -29,7 +37,11 @@ export default meta;
 export const DefaultCheckbox = {
   args: {
     title: "Разработка программного обеспечения на заказ",
+<<<<<<< HEAD
     id: "1",
     name: "checkbox",
+=======
+    value: true,
+>>>>>>> 2895b2c (Сделала компонент Checkbox и storybook к нему)
   },
 };
