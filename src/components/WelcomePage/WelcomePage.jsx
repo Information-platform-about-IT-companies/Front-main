7af1b5fef8c38932b649f23d662d9f1db83bdeef
vvvolkova-92 <saiviolet@gmail.com>
2023-10-25 00:44:24 +0300

    Link / Сборка компонента

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,12 @@
+import { LinkItem } from "../../UI-KIT/Link/LinkItem";
+
 function WelcomePage() {
-  return <div>welcome-page</div>;
+  // return <div>welcome-page</div>;
+  return (
+    <div>
+      <LinkItem url="/test" title="test" />{" "}
+    </div>
+  );
 }
 
 export default WelcomePage;
