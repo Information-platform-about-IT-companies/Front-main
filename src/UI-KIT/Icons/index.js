391d056c6640ce679057e8e1170d720d5511d811
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-10-24 18:17:22 +0300

    добавить иконку Bullet для списков и хлебных крошек

--- src/UI-KIT/Icons/index.js
+++ src/UI-KIT/Icons/index.js
@@ -2,12 +2,21 @@ import { useMemo } from "react";
 
 import IconAccount from "./IconAccount";
 import IconArrow from "./IconArrow";
+import IconBullet from "./IconBullet";
 import IconOctopusArrow from "./IconOctopusArrow";
 import IconPin from "./IconPin";
 import IconRound from "./IconRound";
 import IconSearch from "./IconSearch";
 
-const icons = { IconAccount, IconArrow, IconOctopusArrow, IconPin, IconRound, IconSearch };
+const icons = {
+  IconAccount,
+  IconArrow,
+  IconBullet,
+  IconOctopusArrow,
+  IconPin,
+  IconRound,
+  IconSearch,
+};
 
 export default function Icon({ icon, ...props }) {
   const RenderIcon = useMemo(() => icons[icon], [icon]);
