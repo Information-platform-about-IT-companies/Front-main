2197c8be6bb61d5adb5a6b60926967c659a782b4
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-10-24 18:17:22 +0300

    добавить иконку Bullet для списков и хлебных крошек

--- src/UI-KIT/Icons/index.stories.jsx
+++ src/UI-KIT/Icons/index.stories.jsx
@@ -14,11 +14,12 @@ const meta = {
   argTypes: {
     icon: {
       description:
-        "Обязательный параметр объявления используемой иконки. Может принимать значения - IconAccount | IconArrow | IconOctopusArrow | IconPin | IconRound | IconSearch",
+        "Обязательный параметр объявления используемой иконки. Может принимать значения - IconAccount | IconArrow | IconBullet | IconOctopusArrow | IconPin | IconRound | IconSearch",
       control: { type: "select" },
       options: [
         "IconAccount",
         "IconArrow",
+        "IconBullet",
         "IconOctopusArrow",
         "IconPin",
         "IconRound",
@@ -27,7 +28,7 @@ const meta = {
     },
     size: {
       description:
-        "Необязательный параметр размера иконки. По умолчанию задан стандартный размер 32px, кроме иконки OctopusArrow у которой стандартный размер 60px. Может принимать значения - 24 | 32 | 60",
+        "Необязательный параметр размера иконки. По умолчанию стандартный размер 32px, кроме: иконки Bullet у который стандартный размер 24px и иконки OctopusArrow у которой стандартный размер 60px. Может принимать значения - 24 | 32 | 60",
       control: { type: "inline-radio" },
       options: ["24", "32", "60"],
     },
@@ -61,6 +62,16 @@ export const IconArrow = {
   args: {
     icon: "IconArrow",
     size: 32,
+    color: "black",
+    extClassName: "forStory",
+    direction: "up",
+  },
+};
+
+export const IconBullet = {
+  args: {
+    icon: "IconBullet",
+    size: 24,
     color: "black",
     extClassName: "forStory",
     direction: "up",
