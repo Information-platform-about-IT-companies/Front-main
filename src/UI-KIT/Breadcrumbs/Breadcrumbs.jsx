1e095987ee3bd211c88a2c413f6af7bf93177fa2
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-10-27 16:56:26 +0300

    создать хлебные крошки

--- src/UI-KIT/Breadcrumbs/Breadcrumbs.jsx
+++ src/UI-KIT/Breadcrumbs/Breadcrumbs.jsx
@@ -1,32 +1,36 @@
 import { Link, useLocation } from "react-router-dom";
 
 import "./Breadcrumbs.scss";
-import PropTypes from "prop-types";
 
-export default function Breadcrumbs(props) {
+export default function Breadcrumbs() {
   const location = useLocation();
   let currentLink = "";
+
   const crumbs = location.pathname
     .split("/")
     .filter((crumb) => crumb !== "")
     .map((crumb) => {
-      currentLink = +`/${crumb}`;
+      currentLink += `/${crumb}`;
 
       return (
-        <ul className="breadcrumbs" key={crumb}>
-          <li className="breadcrumbs__element">
-            <Link to={currentLink} className="breadcrumbs__link">
-              {crumb}
-            </Link>
-          </li>
-        </ul>
+        <li className="breadcrumbs__element" key={crumb}>
+          <Link to={currentLink} className="breadcrumbs__link">
+            {crumb}
+          </Link>
+        </li>
       );
     });
-}
 
-Breadcrumbs.propTypes = {
-  main: PropTypes.shape({
-    name: PropTypes.string,
-    link: PropTypes.string,
-  }).isRequired,
-};
+  if (location.pathname !== "/") {
+    return (
+      <ul className="breadcrumbs">
+        <li className="breadcrumbs__element" key="main">
+          <Link to="/" className="breadcrumbs__link">
+            Главная
+          </Link>
+        </li>
+        {crumbs}
+      </ul>
+    );
+  }
+}
