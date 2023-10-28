95727c081e6fd3ca261ac6fd08e502a575c8e398
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-10-28 11:00:51 +0300

    поправить stories

--- src/UI-KIT/Breadcrumbs/Breadcrumbs.stories.jsx
+++ src/UI-KIT/Breadcrumbs/Breadcrumbs.stories.jsx
@@ -1,66 +1,35 @@
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 
 import Breadcrumbs from "./Breadcrumbs";
 import "./Breadcrumbs.scss";
 import "../../index.scss";
+import "../../assets/style/main.scss";
+import Main from "../../components/Main/Main";
+// import WelcomePage from "components/WelcomePage/WelcomePage";
 
-const meta = {
+export default {
   title: "UI-KIT/Breadcrumbs",
   component: Breadcrumbs,
   tags: ["autodocs"],
   parameters: {
     docs: {
       description: {
-        component: "Хлебные крошки на карточке компании",
+        component: "Хлебные крошки",
       },
-    },
-  },
-  argTypes: {
-    main: {
-      description: "Обязательный параметр. Объект со свойствами name и link",
-      control: "object",
-    },
-    services: {
-      description: "Обязательный параметр. Объект со свойствами name и link",
-      control: "object",
-    },
-    city: {
-      description: "Обязательный параметр. Объект со свойствами name и link",
-      control: "object",
-    },
-    company: {
-      description: "Обязательный параметр. Объект со свойствами name и link",
-      control: "object",
-    },
-  },
-};
-
-export default meta;
-
-export const BreadcrumbsExample = {
-  args: {
-    main: {
-      name: "Главная",
-      link: "#",
-    },
-    services: {
-      name: "Веб-разработка",
-      link: "#",
-    },
-    city: {
-      name: "Таганрог",
-      link: "#",
-    },
-    company: {
-      name: "Mentalstack",
-      link: "#",
     },
   },
   decorators: [
     (Story) => (
-      <MemoryRouter initialEntries={["/"]}>
+      <MemoryRouter initialEntries={["/home"]}>
         <Story />
+        <Routes>
+          <Route path="home" element={<Main />} />
+        </Routes>
       </MemoryRouter>
     ),
   ],
+};
+
+export const BreadcrumbsExample = {
+  args: {},
 };
