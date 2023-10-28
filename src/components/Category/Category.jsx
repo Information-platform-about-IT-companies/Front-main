d67cb89e235724b11a9c4f50d461f3b538bf91a2
Alina Sultanova <sultanova1859ts@gmail.com>
2023-10-28 13:33:04 +0500

    Добавила блок и storybook Category

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,30 @@
+import "./Category.scss";
+import PropTypes from "prop-types";
+
+import { CategoryTitle } from "UI-KIT/CategoryTitle/CategoryTitle";
+import { LinkItem } from "UI-KIT/Link/LinkItem";
+
+export function Category({ categories, categoryTitle, icon }) {
+  return (
+    <>
+      <CategoryTitle title={categoryTitle} icon={icon} />
+      <ul className="category">
+        {categories.map((category, i) => (
+          <li className="category_link" key={category.title}>
+            <LinkItem title={category.title} withBullet url="/" />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+Category.propTypes = {
+  categoryTitle: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
