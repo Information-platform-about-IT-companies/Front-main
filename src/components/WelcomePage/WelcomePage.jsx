c918f4550fe8d061cbc63f41562ee245443d8d79
vvvolkova-92 <saiviolet@gmail.com>
2023-10-31 00:00:06 +0300

    Button / Добавлен функционал ссылки, дописаны истории

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -1,7 +1,13 @@
 import "./WelcomePage.scss";
+import { Button } from "../../UI-KIT/Button/Button";
 
 function WelcomePage() {
-  return <div>welcome-page</div>;
+  return (
+    <div>
+      <Button size="standard" fill title="Это кнопка" />
+      <Button url="/" size="standard" fill title="Это ссылка" />
+    </div>
+  );
 }
 
 export default WelcomePage;
