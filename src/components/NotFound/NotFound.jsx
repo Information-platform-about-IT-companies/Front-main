65fe35c8b9fb6361a51c83f83a36ad58ad1b79cf
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-10-31 22:20:15 +0300

    сверстать страницу 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
+import "./NotFound.scss";
+
+import { Button } from "UI-KIT/Button/Button";
+
 function NotFound() {
-  return <div>NotFound-page</div>;
+  return (
+    <main className="not-found">
+      <h1 className="not-found__header">404</h1>
+      <p className="not-found__paragraph">Страница не найдена</p>
+      <Button title="На главную" fill={false} size="standard" url="/" />
+    </main>
+  );
 }
 
 export default NotFound;
