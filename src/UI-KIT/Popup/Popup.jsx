fb018e298903b516a36de8b066ee9d0113f846c7
Alina Sultanova <sultanova1859ts@gmail.com>
2023-10-31 16:16:44 +0500

    Сделала компонент Popup и storybook к нему

---
+++ src/UI-KIT/Popup/Popup.jsx
@@ -0,0 +1,45 @@
+import Icon from "UI-KIT/Icons";
+import "./Popup.scss";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const portal = document.getElementById("portal");
+
+export function Popup({ title, isOpen, onClose, extClassName, children }) {
+  return ReactDOM.createPortal(
+    <div className={`popup ${extClassName} ${isOpen ? "popup_opened" : ""} `}>
+      <div className="popup_container">
+        <div className="popup_header">
+          <div className="popup_logo" />
+          <span
+            role="button"
+            tabIndex={0}
+            className="popup_close-btn"
+            onClick={onClose}
+            onKeyDown={onClose}
+            aria-label="Закрыть модальное окно"
+          />
+        </div>
+
+        <p className="popup_title">{title}</p>
+        <div className="popup_details">{children}</div>
+      </div>
+    </div>,
+    portal,
+  );
+}
+
+Popup.propTypes = {
+  title: PropTypes.string.isRequired,
+  isOpen: PropTypes.func,
+  onClose: PropTypes.func,
+  extClassName: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Popup.defaultProps = {
+  extClassName: "",
+  isOpen: false,
+  onClose: null,
+};
