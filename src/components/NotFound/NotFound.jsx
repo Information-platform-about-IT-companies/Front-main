899dfe8d2143b9804be7bdef8862d222a6f99d04
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-11-01 14:22:39 +0300

    превратить кнопку «на главную» в условную назад/на главную

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,15 +1,35 @@
-import "./NotFound.scss";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import { Button } from "UI-KIT/Button/Button";
 
-function NotFound() {
+import "./NotFound.scss";
+
+export default function NotFound() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const isSameSite =
+    document.referrer.includes(window.location.origin) || location.state?.fromSameSite;
+
+  console.log(isSameSite);
+
+  const handleButtonNotFound = () => {
+    if (isSameSite) {
+      navigate(-1);
+    }
+    navigate("/");
+  };
+
   return (
     <main className="not-found">
       <h1 className="not-found__header">404</h1>
       <p className="not-found__paragraph">Страница не найдена</p>
-      <Button title="На главную" fill={false} size="standard" url="/" />
+      <Button
+        title={isSameSite ? "Назад" : "На главную"}
+        fill={false}
+        size="standard"
+        onClick={handleButtonNotFound}
+      />
     </main>
   );
 }
-
-export default NotFound;
