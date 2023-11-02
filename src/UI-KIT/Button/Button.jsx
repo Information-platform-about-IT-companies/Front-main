de2b8cde44ba3439927883f2462e2a424ce2c56a
vvvolkova-92 <saiviolet@gmail.com>
2023-11-02 15:08:15 +0300

    BUTTON / Добавлен вариант реализациия для navlink, изменена стори

--- src/UI-KIT/Button/Button.jsx
+++ src/UI-KIT/Button/Button.jsx
@@ -1,9 +1,41 @@
 import "./Button.scss";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-export function Button({ url, size, fill, title, onClick, extClassName, ...props }) {
-  const button = (
+export function Button({ size, fill, title, linkType, url, onClick, extClassName, ...props }) {
+  if (url) {
+    const isLink = linkType === "link";
+    const isNavLink = linkType === "navlink";
+    const link = isLink && (
+      <Link
+        to={url}
+        className={
+          fill
+            ? `button button_style_fill button_size_${size} ${extClassName}`
+            : `button button_style_outline button_size_${size} ${extClassName}`
+        }
+        {...props}
+      >
+        {title}
+      </Link>
+    );
+
+    const navlink = isNavLink && (
+      <NavLink
+        to={url}
+        className={
+          fill
+            ? `button button_style_fill button_size_${size} ${extClassName}`
+            : `button button_style_outline button_size_${size} ${extClassName}`
+        }
+        {...props}
+      >
+        {title}
+      </NavLink>
+    );
+    return isLink ? link : navlink;
+  }
+  return (
     <button
       onClick={onClick}
       className={
@@ -16,24 +48,10 @@ export function Button({ url, size, fill, title, onClick, extClassName, ...props
       {title}
     </button>
   );
-  const link = (
-    <Link
-      to={url}
-      className={
-        fill
-          ? `button button_style_fill button_size_${size} ${extClassName}`
-          : `button button_style_outline button_size_${size} ${extClassName}`
-      }
-      {...props}
-    >
-      {title}
-    </Link>
-  );
-
-  return url ? link : button;
 }
 
 Button.propTypes = {
+  linkType: PropTypes.string,
   url: PropTypes.string,
   size: PropTypes.string.isRequired,
   fill: PropTypes.bool.isRequired,
@@ -43,7 +61,8 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
+  linkType: null,
   onClick: null,
   url: null,
-  extClassName: null,
+  extClassName: "",
 };
