a4e84d50ec0487d33ee1494faaa7608041034ad3
vvvolkova-92 <saiviolet@gmail.com>
2023-11-05 18:25:29 +0300

    Общая структура страницы

--- src/pages/FilterPage/FilterPage.jsx
+++ src/pages/FilterPage/FilterPage.jsx
@@ -39,6 +39,33 @@ function FilterPage() {
             description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae laborum nam nobis reprehenderit, tempora? Ab dolor dolorum esse, explicabo fugiat labore molestiae nulla officiis, omnis pariatur quaerat quasi quibusdam rerum suscipit veritatis. Accusamus, accusantium enim iusto libero magnam neque nihil porro praesentium qui ratione vero voluptate! Quia, quos similique?"
           />
         </li>
+        <li className="filterPage__listitem">
+          <CompanyCard
+            services={[
+              "Исчезновение Статуи Свободы",
+              "Полет перед зрителями на сцене",
+              "Проход вскозь Великую китайскую стену",
+              "Разрезание ассистентки",
+              "блаблабла",
+            ]}
+            name="Дэвид Блейн"
+            iconLikeState
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae laborum nam nobis reprehenderit, tempora? Ab dolor dolorum esse, explicabo fugiat labore molestiae nulla officiis, omnis pariatur quaerat quasi quibusdam rerum suscipit veritatis. Accusamus, accusantium enim iusto libero magnam neque nihil porro praesentium qui ratione vero voluptate! Quia, quos similique?"
+          />
+        </li>
+        <li>
+          <CompanyCard
+            services={[
+              "Разрезание ассистентки",
+              "Исчезновение Статуи Свободы",
+              "блаблабла",
+              "Полет перед зрителями на сцене",
+              "Проход вскозь Великую китайскую стену",
+            ]}
+            name="Дэвид Коперфильд"
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi beatae laborum nam nobis reprehenderit, tempora? Ab dolor dolorum esse, explicabo fugiat labore molestiae nulla officiis, omnis pariatur quaerat quasi quibusdam rerum suscipit veritatis. Accusamus, accusantium enim iusto libero magnam neque nihil porro praesentium qui ratione vero voluptate! Quia, quos similique?"
+          />
+        </li>
       </ul>
       <div>тут будет пагинация</div>
     </main>
--- src/services/constants.js
+++ src/services/constants.js
@@ -18,3 +18,52 @@ export function cutText(text, limit) {
   if (text.length < limit) return text;
   return `${text.slice(0, limit)}...`;
 }
+export const hardcode = {
+  requests: ["Веб-дизайн", "Разработка для Android", "Москва"],
+  companies: [
+    {
+      title: "Веб-разработка",
+      comp: [
+        { title: "Бла-бла-бла-1", link: "/bla-1" },
+        { title: "Бла-бла-бла-1", link: "/bla-1" },
+        { title: "Бла-бла-бла-2", link: "/bla-2" },
+        { title: "Бла-бла-бла-3", link: "/bla-3" },
+        { title: "Бла-бла-бла-4", link: "/bla-4" },
+        { title: "Бла-бла-бла-5", link: "/bla-5" },
+      ],
+    },
+    {
+      title: "Разработка ПО",
+      comp: [
+        { title: "Бу-бу-бу-1", link: "/boo-1" },
+        { title: "Бу-бу-бу-2", link: "/boo-2" },
+        { title: "Бу-бу-бу-3", link: "/boo-3" },
+        { title: "Бу-бу-бу-4", link: "/boo-4" },
+        { title: "Бу-бу-бу-5", link: "/boo-5" },
+        { title: "Бу-бу-бу-6", link: "/boo-6" },
+      ],
+    },
+    {
+      title: "Дизайн",
+      comp: [
+        { title: "Бу-бу-бу-1", link: "/boo-1" },
+        { title: "Бу-бу-бу-2", link: "/boo-2" },
+        { title: "Бу-бу-бу-3", link: "/boo-3" },
+        { title: "Бу-бу-бу-4", link: "/boo-4" },
+        { title: "Бу-бу-бу-5", link: "/boo-5" },
+        { title: "Бу-бу-бу-6", link: "/boo-6" },
+      ],
+    },
+    {
+      title: "Маркетинг",
+      comp: [
+        { title: "Бла-бла-бла-1", link: "/bla-1" },
+        { title: "Бла-бла-бла-1", link: "/bla-1" },
+        { title: "Бла-бла-бла-2", link: "/bla-2" },
+        { title: "Бла-бла-бла-3", link: "/bla-3" },
+        { title: "Бла-бла-бла-4", link: "/bla-4" },
+        { title: "Бла-бла-бла-5", link: "/bla-5" },
+      ],
+    },
+  ],
+};
