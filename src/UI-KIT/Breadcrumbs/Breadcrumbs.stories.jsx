b08529ff95d4f5ff051109e812073364f4f4a1d9
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-11-05 10:36:12 +0300

    убрать из хлебных крошек верстку главной страницы

--- src/UI-KIT/Breadcrumbs/Breadcrumbs.stories.jsx
+++ src/UI-KIT/Breadcrumbs/Breadcrumbs.stories.jsx
@@ -1,9 +1,9 @@
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { MemoryRouter, Routes, Route, useNavigate } from "react-router-dom";
 
+import { Button } from "UI-KIT/Button/Button";
 import Breadcrumbs from "./Breadcrumbs";
+
 import "./Breadcrumbs.scss";
-import Main from "../../pages/Main/Main";
-// import WelcomePage from "components/WelcomePage/WelcomePage";
 
 export default {
   title: "UI-KIT/Breadcrumbs",
@@ -18,10 +18,90 @@ export default {
   },
   decorators: [
     (Story) => (
-      <MemoryRouter initialEntries={["/home"]}>
+      <MemoryRouter initialEntries={["/frontend"]}>
         <Story />
         <Routes>
-          <Route path="home" element={<Main />} />
+          <Route
+            path="/"
+            element={
+              <Button
+                size="standard"
+                title="Команда фронтенда"
+                url="/frontend"
+                linkType="navlink"
+              />
+            }
+          />
+          <Route
+            path="frontend"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <br />
+                <Button size="standard" title="Артур" url="/frontend/artur" linkType="navlink" />
+                <Button size="standard" title="Алина" url="/frontend/alina" linkType="navlink" />
+                <Button
+                  size="standard"
+                  title="Виолета"
+                  url="/frontend/violetta"
+                  linkType="navlink"
+                />
+                <Button
+                  size="standard"
+                  title="Евгений"
+                  url="/frontend/evgeniy"
+                  linkType="navlink"
+                />
+                <Button size="standard" title="Никита" url="/frontend/nikita" linkType="navlink" />
+              </>
+            }
+          />
+          <Route
+            path="/frontend/artur"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <Button size="standard" title="Назад " url="/frontend" linkType="navlink" />
+              </>
+            }
+          />
+          <Route
+            path="/frontend/alina"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <Button size="standard" title="Назад " url="/frontend" linkType="navlink" />
+              </>
+            }
+          />
+          <Route
+            path="/frontend/violetta"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <Button size="standard" title="Назад " url="/frontend" linkType="navlink" />
+              </>
+            }
+          />
+          <Route
+            path="/frontend/nikita"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <Button size="standard" title="Назад " url="/frontend" linkType="navlink" />
+              </>
+            }
+          />
+
+          <Route
+            path="/frontend/evgeniy"
+            element={
+              <>
+                <Button size="standard" title="На главную" url="/" linkType="navlink" />
+                <Button size="standard" title="Назад " url="/frontend" linkType="navlink" />
+              </>
+            }
+          />
         </Routes>
       </MemoryRouter>
     ),
