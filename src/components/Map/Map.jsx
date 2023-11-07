d0d592b7d007d78d31fe3335146cce7d0006ddbd
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-11-07 20:22:59 +0300

    увязка с develop

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Map({ address }) {
+  const [latitude, setLatitude] = React.useState(null);
+  const [longitude, setLongitude] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const position = [latitude, longitude];
+
+  async function geocodeAddress() {
+    try {
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json`,
+      );
+      const data = await response.json();
+
+      if (data.length > 0) {
+        const result = data[0];
+        setLatitude(parseFloat(result.lat));
+        setLongitude(parseFloat(result.lon));
+      } else {
+        setError(`Адрес "${address}" не найден.`);
+      }
+    } catch (err) {
+      setError(`Произошла ошибка при выполнении запроса: ${err.message}`);
+    }
+  }
+
+  React.useEffect(() => {
+    geocodeAddress();
+  }, [address]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (position === null || longitude === null) {
+    return null; // Пока координаты не получены, компонент не будет рендериться
+  }
+
+  return (
+    <MapContainer center={position} zoom={15} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>{address}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
