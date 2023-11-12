22160cff7847b0babc41c83c88bbd3358380b4a4
Alina Sultanova <sultanova1859ts@gmail.com>
2023-11-12 10:44:22 +0500

    refactored icons

--- src/UI-KIT/CategoryTitle/CategoryTitle.jsx
+++ src/UI-KIT/CategoryTitle/CategoryTitle.jsx
@@ -1,7 +1,23 @@
 import "./CategoryTitle.scss";
 import PropTypes from "prop-types";
+import IconСategoryWeb from "UI-KIT/Icons/IconСategoryWeb";
+import IconСategorySoftware from "UI-KIT/Icons/IconСategorySoftware";
+import IconСategoryDesign from "UI-KIT/Icons/IconСategoryDesign";
+import IconСategoryMarketing from "UI-KIT/Icons/IconСategoryMarketing";
 
-export function CategoryTitle({ title, icon }) {
+export function CategoryTitle({ title, id }) {
+  let icon;
+
+  if (id === "1") {
+    icon = <IconСategoryWeb />;
+  } else if (id === "2") {
+    icon = <IconСategorySoftware />;
+  } else if (id === "3") {
+    icon = <IconСategoryDesign />;
+  } else {
+    icon = <IconСategoryMarketing />;
+  }
+
   return (
     <span className="category_title">
       {icon}
@@ -12,5 +28,5 @@ export function CategoryTitle({ title, icon }) {
 
 CategoryTitle.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.node.isRequired,
+  id: PropTypes.string.isRequired,
 };
--- src/services/constants.js
+++ src/services/constants.js
@@ -23,6 +23,7 @@ export const hardcode = {
   companies: [
     {
       title: "Веб-разработка",
+      id: "1",
       comp: [
         { title: "Веб-разработка", link: "/bla-1" },
         { title: "Разработка под e-commerce", link: "/bla-1" },
@@ -40,6 +41,7 @@ export const hardcode = {
     },
     {
       title: "Разработка ПО",
+      id: "2",
       comp: [
         { title: "Разработка для IOS", link: "/boo-1" },
         { title: "Разработка для Android", link: "/boo-2" },
@@ -56,6 +58,7 @@ export const hardcode = {
     },
     {
       title: "Дизайн",
+      id: "3",
       comp: [
         { title: "Веб-дизайн", link: "/boo-1" },
         { title: "UX/UI ", link: "/boo-2" },
@@ -68,6 +71,7 @@ export const hardcode = {
     },
     {
       title: "Маркетинг",
+      id: "4",
       comp: [
         { title: "Реклама", link: "/bla-1" },
         { title: "SMM", link: "/bla-2" },
