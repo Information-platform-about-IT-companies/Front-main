2f89626d71fab856734b7387d5732ef7e68fdece
gineff <canone@inbox.ru>
2023-11-20 18:20:03 +0300

    добавить контекст в тесты App.test.js

--- src/pages/App/App.test.js
+++ src/pages/App/App.test.js
@@ -2,13 +2,16 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
+import MainContextProvider from "context/MainContext";
 import App from "./App";
 
 it("renders welcome message", () => {
   render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <MainContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MainContextProvider>,
   );
   expect(screen.getByText(/Найдите подходящие для вас IT-компании в России/i)).toBeInTheDocument();
 });
