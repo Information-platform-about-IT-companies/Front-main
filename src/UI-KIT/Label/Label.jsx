31532b767edf8762b9ebdf1a83f395f8bfa8c2ac
Alina Sultanova <sultanova1859ts@gmail.com>
2023-11-27 22:04:43 +0500

    рефактор

--- src/UI-KIT/Label/Label.jsx
+++ src/UI-KIT/Label/Label.jsx
@@ -1,21 +1,15 @@
 import "./Label.scss";
 import PropTypes from "prop-types";
 
-export function Label({ title, extClassName, tooltip }) {
-  return (
-    <span className={`label ${extClassName}`} data-tooltip={tooltip}>
-      {title}
-    </span>
-  );
+export function Label({ title, extClassName }) {
+  return <span className={`label ${extClassName}`}>{title}</span>;
 }
 
 Label.propTypes = {
   title: PropTypes.string.isRequired,
   extClassName: PropTypes.string,
-  tooltip: PropTypes.string,
 };
 
 Label.defaultProps = {
   extClassName: "",
-  tooltip: "",
 };
