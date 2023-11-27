7e6f2106a594f5fdb449dd94cd998aa4fe71586b
Alina Sultanova <sultanova1859ts@gmail.com>
2023-11-27 21:59:24 +0500

    Добавила всплывашки на лейблы

--- src/UI-KIT/Label/Label.jsx
+++ src/UI-KIT/Label/Label.jsx
@@ -1,15 +1,21 @@
 import "./Label.scss";
 import PropTypes from "prop-types";
 
-export function Label({ title, extClassName }) {
-  return <span className={`label ${extClassName}`}>{title}</span>;
+export function Label({ title, extClassName, tooltip }) {
+  return (
+    <span className={`label ${extClassName}`} data-tooltip={tooltip}>
+      {title}
+    </span>
+  );
 }
 
 Label.propTypes = {
   title: PropTypes.string.isRequired,
   extClassName: PropTypes.string,
+  tooltip: PropTypes.string,
 };
 
 Label.defaultProps = {
   extClassName: "",
+  tooltip: "",
 };
--- src/pages/App/App.test.jsx
+++ src/pages/App/App.test.jsx
@@ -4,6 +4,8 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import MainContextProvider from "context/MainContext";
 import App from "./App";
+import { HTTP } from "api/http";
+import ThemeProvider from "context/ThemeContext";
 
 function LocationDisplay() {
   const location = useLocation();
