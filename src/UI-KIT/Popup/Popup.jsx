d3702269df57bfba9552c196e88dd19c1107781f
gineff <canone@inbox.ru>
2023-12-03 00:29:06 +0300

    добавить функционал закрытия по клику или нажатию

--- src/UI-KIT/Popup/Popup.jsx
+++ src/UI-KIT/Popup/Popup.jsx
@@ -1,15 +1,38 @@
-import Icon from "UI-KIT/Icons";
 import "./Popup.scss";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 const portal = document.getElementById("portal");
 
 export function Popup({ title, isOpen, onClose, extClassName, children }) {
+  const popupRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   return ReactDOM.createPortal(
     <div className={`popup ${extClassName} ${isOpen ? "popup_opened" : ""} `}>
-      <div className="popup_container">
+      <div ref={popupRef} className="popup_container">
         <div className="popup_header">
           <div className="popup_logo" />
           <button
