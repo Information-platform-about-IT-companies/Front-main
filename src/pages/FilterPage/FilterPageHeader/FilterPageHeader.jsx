0aafd2cfadbddff86d8fa944263f21b756fada2f
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-12-04 18:52:09 +0300

    поправить не активируемую кнопку

--- src/pages/FilterPage/FilterPageHeader/FilterPageHeader.jsx
+++ src/pages/FilterPage/FilterPageHeader/FilterPageHeader.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { cityIn, cityFrom } from "lvovich";
+import { cityIn } from "lvovich";
 // функции
 import { declinationsNumericalValues } from "services/constants";
 import findServiceById from "services/findServiceById";
